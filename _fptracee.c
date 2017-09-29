30ed0c2288ceaf4cc6dbedd30a5721f90fd6feba
Orivej Desh <orivej@gmx.fr>
2017-09-29 05:17:56 +0000

    Simplify tracee

--- _fptracee.c
+++ _fptracee.c
@@ -1,6 +1,5 @@
 #include <signal.h>
 #include <stdio.h>
-#include <string.h>
 #include <sys/ptrace.h>
 #include <unistd.h>
 
@@ -9,15 +8,10 @@ int main(int argc, char **argv) {
         fputs("Arguments: program args...\n", stderr);
         return 1;
     }
-
-    char *args[argc];
-    memcpy(args, argv + 1, (argc - 1) * sizeof(char *));
-    args[argc - 1] = NULL;
-
     if (ptrace(PTRACE_TRACEME)) {
         perror("ptrace failed");
     }
     raise(SIGSTOP);
-    execvp(args[0], args);
+    execvp(argv[1], argv + 1);
     perror("execvp failed");
 }
