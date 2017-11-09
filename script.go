b0bb459b87f61434b51e9f19932740b651a25e4b
Orivej Desh <orivej@gmx.fr>
2017-11-09 13:29:36 +0000

    Write scripts that execute by an absolute path
    Sometimes the executable path may not be inferred from the current directory and PATH.

--- script.go
+++ script.go
@@ -16,13 +16,19 @@ func writeScript(dir string, cmd Cmd) {
 	e.Exit(err)
 	defer e.CloseOrPrint(f)
 
+	sh, exec, cmdline := "#!/bin/sh", "exec", cmd.Args
+	if cmd.Args[0] != cmd.Path {
+		sh = "#!/usr/bin/env bash"
+		exec = "exec -a " + shquote.Quote(cmd.Args[0])
+		cmdline = append([]string{cmd.Path}, cmd.Args[1:]...)
+	}
 	buf := bufio.NewWriter(f)
-	fmt.Fprintln(buf, "#!/bin/sh")
+	fmt.Fprintln(buf, sh)
 	fmt.Fprintf(buf, "\ncd %s\n", shquote.Quote(cmd.Dir))
 	if len(cmd.Env) != 0 {
 		fmt.Fprintf(buf, "\nexport %s\n", shquote.QuoteList(cmd.Env))
 	}
-	fmt.Fprintf(buf, "\n${exec:-exec} %s \"$@\"\n", shquote.QuoteList(cmd.Args))
+	fmt.Fprintf(buf, "\n${exec:-%s} %s \"$@\"\n", exec, shquote.QuoteList(cmdline))
 	err = buf.Flush()
 	e.Exit(err)
 }
